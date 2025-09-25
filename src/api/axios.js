2f4e53f90f4ce39e0c5a5e0477e2e5381b3fbeb7
subrato <subrato2498@gmial.com>
2025-09-26 01:58:04 +0530

    updated

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
+    baseURL: 'https://smartgro-production.up.railway.app',
     headers: { 'Content-Type': 'application/json' }
 });
 
